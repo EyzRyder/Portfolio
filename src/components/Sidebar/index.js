b0302ac8a1baff3f09ac215e51078d3c00d58dd6
Gabriel Bessi <85580011+EyzRyder@users.noreply.github.com>
2022-07-20 19:13:08 -0300

    update nav

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -13,10 +13,14 @@ const Sidebar = () => {
 
   function myFunction() {
     var x = document.getElementById("nav-itens");
+    var y = document.getElementById("mobile-link");
+
     if (x.className.indexOf("show") == -1) {
       x.className += "show";
+      y.className += "cross";
     } else {
       x.className = x.className.replace("show", "");
+      y.className = y.className.replace("cross", "");
     }
   }
 
