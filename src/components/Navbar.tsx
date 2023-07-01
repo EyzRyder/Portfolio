e235eafd5e61312890913b93d751da00e75a39eb
Gabriel Bessi <85580011+EyzRyder@users.noreply.github.com>
2023-07-01 19:26:14 -0300

    main part

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,35 @@
+import Image from 'next/image'
+import logo from '@/assets/mainLogoNoShine.svg'
+import { FileCode, Mail, User2 } from 'lucide-react'
+// import Link from 'next/link'
+
+export default function Navbar() {
+  return (
+    <aside className="fixed top-1/2 z-10 flex -translate-y-1/2 flex-col justify-center gap-2 rounded-br-2xl rounded-tr-2xl bg-zinc-600 px-3 py-6">
+      <a
+        href="#home"
+        className="flex h-12 w-12 items-center justify-center transition-all hover:rounded-full hover:bg-primary"
+      >
+        <Image src={logo} alt="home button" className="w-9" />
+      </a>
+      <a
+        href="#about"
+        className="flex h-12 w-12 items-center justify-center transition-all hover:rounded-full hover:bg-primary"
+      >
+        <User2 />
+      </a>
+      <a
+        href="#work"
+        className="flex h-12 w-12 items-center justify-center transition-all hover:rounded-full hover:bg-primary"
+      >
+        <FileCode />
+      </a>
+      <a
+        href="#contact"
+        className="flex h-12 w-12 items-center justify-center transition-all hover:rounded-full hover:bg-primary"
+      >
+        <Mail />
+      </a>
+    </aside>
+  )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,21 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: 'var(--font-poppins)',
+        alt: 'var(--font-cherry)',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors: {
+        'bg-main': '#430220',
+        primary: '#FFD700',
+        'primary-tint': '#E8A21F',
+        secondary: '#6C162B',
+        tertiary: '#B8522E',
       },
     },
   },
