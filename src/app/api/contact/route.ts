865c9d687abc6c9a70c5692f359f2eddfef29e32
Gabriel Bessi <85580011+EyzRyder@users.noreply.github.com>
2023-07-14 23:14:04 +0000

    type fixing

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { mailOptions, transporter } from '@/libs/nodemailer'
 import { NextRequest, NextResponse } from 'next/server'
 
--- src/useHook/useMousePosition.tsx
+++ src/useHook/useMousePosition.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import { useEffect, useState } from 'react'
 const useMousePosition = () => {
   const [mousePosition, setMousePosition] = useState({ x: null, y: null })
